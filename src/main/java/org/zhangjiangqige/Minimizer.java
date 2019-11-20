8ea5a0bfbdb1c3e131b12221df3d7beb17f06f76
zhangjiangqige <zhangjiangqige@gmail.com>
2019-11-20 17:23:18 -0500

    removes private/package private constructors/methods/fields, bodies of constructors, initializers of fields, and changes `@Deprecated(since="...")` to `@Deprecated`

--- src/main/java/org/zhangjiangqige/Minimizer.java
+++ src/main/java/org/zhangjiangqige/Minimizer.java
@@ -2,9 +2,13 @@ package org.zhangjiangqige;
 
 import com.github.javaparser.ParseResult;
 import com.github.javaparser.ParserConfiguration;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.MemberValuePair;
+import com.github.javaparser.ast.expr.NormalAnnotationExpr;
+import com.github.javaparser.ast.nodeTypes.modifiers.NodeWithAccessModifiers;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.utils.ParserCollectionStrategy;
 import com.github.javaparser.utils.ProjectRoot;
 import com.github.javaparser.utils.SourceRoot;
@@ -12,7 +16,6 @@ import com.github.javaparser.utils.SourceRoot;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -27,6 +30,7 @@ public class Minimizer {
         ParserConfiguration conf = new ParserConfiguration();
         // This removes all comments, by not parsing them at all
         conf.setAttributeComments(false);
+        MinimizationVisitor sm = new MinimizationVisitor();
 
         ProjectRoot projectRoot = new ParserCollectionStrategy().collect(root);
         projectRoot.getSourceRoots().forEach(sourceRoot -> {
@@ -38,7 +42,7 @@ public class Minimizer {
                         Optional<CompilationUnit> opt = result.getResult();
                         if (opt.isPresent()) {
                             CompilationUnit cu = opt.get();
-                            minimize(cu);
+                            sm.visit(cu, null);
                         }
                         return Result.SAVE;
                     }
@@ -49,11 +53,66 @@ public class Minimizer {
         });
     }
 
-    private static void minimize(CompilationUnit cu) {
-        /**
-         * Minimize a compilation unit (a source file) by removing bodies.
-         */
-        cu.findAll(ClassOrInterfaceDeclaration.class)
-                .forEach(cOrI -> cOrI.getMethods().forEach(MethodDeclaration::removeBody));
+    private static class MinimizationVisitor extends ModifierVisitor<Void> {
+        @Override
+        public ClassOrInterfaceDeclaration visit(ClassOrInterfaceDeclaration cid, Void arg) {
+            super.visit(cid, arg);
+            removeIfPrivateOrPkgPrivate(cid);
+            return cid;
+        }
+
+        @Override
+        public ConstructorDeclaration visit(ConstructorDeclaration cd, Void arg) {
+            super.visit(cd, arg);
+            if (!removeIfPrivateOrPkgPrivate(cd)) {
+                // ConstructorDeclaration has to have a body
+                cd.setBody(new BlockStmt());
+            }
+            return cd;
+        }
+
+        @Override
+        public MethodDeclaration visit(MethodDeclaration md, Void arg) {
+            super.visit(md, arg);
+            if (!removeIfPrivateOrPkgPrivate(md))
+                md.removeBody();
+            return md;
+        }
+
+        @Override
+        public FieldDeclaration visit(FieldDeclaration fd, Void arg) {
+            super.visit(fd, arg);
+            if (!removeIfPrivateOrPkgPrivate(fd)) {
+                fd.getVariables().forEach(v -> v.getInitializer().ifPresent(Node::remove));
+            }
+            return fd;
+        }
+
+        @Override
+        public InitializerDeclaration visit(InitializerDeclaration id, Void arg) {
+            super.visit(id, arg);
+            id.remove();
+            return id;
+        }
+
+        @Override
+        public NormalAnnotationExpr visit(NormalAnnotationExpr nae, Void arg) {
+            super.visit(nae, arg);
+            NodeList<MemberValuePair> empty = new NodeList<>();
+            if (nae.getNameAsString().equals("Deprecated")) {
+                nae.setPairs(empty);
+            }
+            return nae;
+        }
+
+        // remove the whole node if it's private or package private
+        private boolean removeIfPrivateOrPkgPrivate(NodeWithAccessModifiers node) {
+            AccessSpecifier as = node.getAccessSpecifier();
+            if (as.equals(AccessSpecifier.PRIVATE) || as.equals(AccessSpecifier.PACKAGE_PRIVATE)) {
+                ((Node)node).remove();
+                return true;
+            }
+            return false;
+        }
     }
 }
